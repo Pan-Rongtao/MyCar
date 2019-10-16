cde608ec135746608cdfd47b34c92f537c8510b1
Pan, Rongtao <Rongtao.Pan@desay-svautomotive.com>
2019-10-16 15:58:51 +0800

    move

--- build_msvc14.bat
+++ build_msvc14.bat
@@ -5,7 +5,7 @@ set build_dir="build"
 if not exist "%build_dir%" md "%build_dir%"
 cd "%build_dir%"
 
-cmake -DCMAKE_INSTALL_PREFIX=../dist -G "Visual Studio 14 2015" ..
+cmake -DCMAKE_INSTALL_PREFIX=../dist/win32 -G "Visual Studio 14 2015" ..
 @%msbuild_execute% INSTALL.vcxproj /p:Configuration="debug"
 @%msbuild_execute% INSTALL.vcxproj /p:Configuration="release"
 
--- dist/etc/run_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set PATH=%PATH%;..\bin\release;..\lib\release
-
-..\bin\release\service
-
-@pause
