f68cc8ba3ddcb048e74fac5a8231a5883f6ea689
Pan <Rongtao.Pan@desay-svautomotive.com>
2019-10-18 11:13:06 +0800

    update

--- dist/arm/bin/release/run_client.sh
+++ dist/arm/bin/release/run_client.sh
@@ -3,5 +3,5 @@ export LD_LIBRARY_PATH=../../lib/release
 export QT_QPA_PLATFORM=wayland
 export QT_QPA_FONTDIR=/svp/share/fonts
 export QT_WAYLAND_SHELL_INTEGRATION=ivi-shell
-export QT_IVI_SURFACE_ID=30000
+export QT_IVI_SURFACE_ID=3001
 ./client
